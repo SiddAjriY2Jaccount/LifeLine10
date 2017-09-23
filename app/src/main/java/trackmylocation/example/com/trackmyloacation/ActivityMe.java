d1775ff9f1c34a63e19e2dadd7789e2fa4dbe735
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2017-09-24 02:32:48 +0530

    First

---
+++ app/src/main/java/trackmylocation/example/com/trackmyloacation/ActivityMe.java
@@ -0,0 +1,13 @@
+package trackmylocation.example.com.trackmyloacation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+    }
+}
